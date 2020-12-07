74edf7dbdf79309eac997cc2b2641fbb47d323a8
ymex <ymex@foxmail.com>
2020-12-07 16:52:52 +0800

    init

---
+++ swiftui.sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui.sample
+//
+//  Created by 一木二小 on 2020/12/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui.sample/swiftui_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_sampleApp.swift
+//  swiftui.sample
+//
+//  Created by 一木二小 on 2020/12/7.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
